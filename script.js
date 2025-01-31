8fdd86bffa4e642e24b01e2332a351388ea7682c
KAnkita-01 <ankitakmohana@gmail.com>
2025-01-31 11:17:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -36,17 +36,17 @@ function sendMessage() {
       "Hello": "Hi there! How can I help you today 😊?",
       "How are you": "I'm doing great, thank you for asking! How about you 😊?",
       "Bye": "Goodbye! Have a nice day 😊!",
-      "Default": "I'm not sure how to respond to that. Could you please rephrase 🤔?"
-      "I'm happy today": "Great, nice to hear that 😀 "
-      "I'm sad today": "Oh no, sad to hear that. May I know what's bothering you. 🥺 "
-      "Thank you": "Thanks for reaching out. Incase you need any help, feel free to reach out to me.😇"
-      "Thank you so much": "Thanks for reaching out. Incase you need any help, feel free to reach out to me. 😇"
-      "Thanks": "Thanks for reaching out. Incase you need any help, feel free to reach out to me. 😇"
-      "Thanks a lot": "Thanks for reaching out. Incase you need any help, feel free to reach out to me. 😇"
-      "Tq": "Thanks for reaching out. Incase you need any help, feel free to reach out to me."
-      "Had your breakfast?": "I'm a robot and I don't eat. Thanks for asking 😅 "
-      "Had your lunch?": "I'm a robot and I don't eat. Thanks for asking 😅"
-      "Had you dinner?": "I'm a robot and I don't eat. Thanks for asking 😅"
+      "Default": "I'm not sure how to respond to that. Could you please rephrase 🤔?",
+      "I'm happy today": "Great, nice to hear that 😀 ",
+      "I'm sad today": "Oh no, sad to hear that. May I know what's bothering you. 🥺 ",
+      "Thank you": "Thanks for reaching out. Incase you need any help, feel free to reach out to me.😇",
+      "Thank you so much": "Thanks for reaching out. Incase you need any help, feel free to reach out to me. 😇",
+      "Thanks": "Thanks for reaching out. Incase you need any help, feel free to reach out to me. 😇",
+      "Thanks a lot": "Thanks for reaching out. Incase you need any help, feel free to reach out to me. 😇",
+      "Tq": "Thanks for reaching out. Incase you need any help, feel free to reach out to me.",
+      "Had your breakfast?": "I'm a robot and I don't eat. Thanks for asking 😅 ",
+      "Had your lunch?": "I'm a robot and I don't eat. Thanks for asking 😅",
+      "Had you dinner?": "I'm a robot and I don't eat. Thanks for asking 😅",
 
     };
   
